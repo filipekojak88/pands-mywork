0b0061dd3acf9142d782e7c55618e95a127073fd
Filipe Carvalho <filipeferc88@gmail.com>
2024-02-16 00:43:27 +0000

    lab3.2

---
+++ week03/lab3.2.3-floor.py
@@ -0,0 +1,8 @@
+# floors a number.
+
+# author: Filipe Carvalho
+import math
+
+numberTofloor = float(input("Enter a float number:"))
+flooredNumber = math.floor(numberTofloor)
+print('{} floored is {}'.format(numberTofloor, flooredNumber))
---
+++ week03/lab3.2.4-extra.py
@@ -0,0 +1,13 @@
+# This program takes a float amount of dollars 
+# and returns that absolute amound in cents
+# author: Filipe Carvalho
+
+
+
+amount = float(input("Please enter an amount:")) # Input in float
+absolute = abs(amount) # Change the float to an absolute value
+
+
+
+outcomes = int (absolute * 100) # Multiply to 100 and transform in interger
+print(f'That amount in cent is: {outcomes}')
