f8daa08b9a826481873e8c5c7ad561dc357ff0f6
Filipe Carvalho <filipeferc88@gmail.com>
2024-04-01 23:52:44 +0100

    week09 lab

---
+++ week09-exceptions/useFib.py
@@ -0,0 +1,8 @@
+# Program useFib.py
+# This program prompts the user to enter a number
+# and returns the Fibonacci sequence.
+# Author: Filipe Carvalho
+import myFunctions 
+
+nTimes = int(input('how many:'))
+print(myFunctions.fibonacci(nTimes))
